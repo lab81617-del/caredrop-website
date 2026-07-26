ff8b7c69511bbb27d65bf5dba167c8fe7c6f7667
lab81617-del <lab81617@gmail.com>
2026-07-26 15:27:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -114,5 +114,8 @@ def admin_logout():
   return redirect(url_for("admin_login"))
 
 
+import os
+
 if __name__ == "__main__":
-  app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
